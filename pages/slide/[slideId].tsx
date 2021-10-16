6518ab65c830cce0b5fc63c10c8cf521d9030ba8
KitaharaMugiro <you@example.com>
2021-10-16 17:21:21 +0900

    スライド同期機能追加

--- pages/slide/[slideId].tsx
+++ pages/slide/[slideId].tsx
@@ -9,44 +9,79 @@ import style from "./style.module.css";
 
 const Page = () => {
     const router = useRouter()
-    const { slideId, admin } = router.query
+    const { slideId, admin: isAdmin } = router.query
 
     //slide状態変数
     const [slideState, setSlideState] = useRealtimeSharedState({
-        picNumber: 0,
-    }, "slideState" + slideId)
+        pageNumber: 0,
+    }, "slideState")
+    const [localPageNumber, setLocalPageNumber] = useState(0)
+    const [isSync, setIsSync] = useState(true)
 
     //データ取得
     const { loading, error, data: initialSlide } = useQuerySlideQuery({ variables: { slideId: Number(slideId) } })
     const slide = initialSlide?.slideshare_Slide_by_pk
     const pages = slide?.Pages
-    const page = pages?.at(slideState.picNumber)
+    const viewingPage = pages?.at(localPageNumber)
+
+    useEffect(() => {
+        if (isSync) {
+            setLocalPageNumber(slideState.pageNumber)
+        }
+    }, [slideState.pageNumber])
 
     const goNext = () => {
-        slideState.picNumber += 1
-        setSlideState(slideState)
+        if (isAdmin) {
+            slideState.pageNumber += 1
+            setSlideState(slideState)
+        } else {
+            const nextPageNumber = localPageNumber + 1
+            setIsSync(slideState.pageNumber === nextPageNumber)
+            setLocalPageNumber(nextPageNumber)
+        }
     }
 
     const goPrevious = () => {
-        slideState.picNumber -= 1
-        setSlideState(slideState)
+        if (isAdmin) {
+            slideState.pageNumber -= 1
+            setSlideState(slideState)
+        } else {
+            const nextPageNumber = localPageNumber - 1
+            setIsSync(slideState.pageNumber === nextPageNumber)
+            setLocalPageNumber(nextPageNumber)
+        }
     }
 
     const goStart = () => {
-        slideState.picNumber = 0
-        setSlideState(slideState)
+        if (isAdmin) {
+            slideState.pageNumber = 0
+            setSlideState(slideState)
+        } else {
+            setLocalPageNumber(0)
+        }
     }
 
     const onChangePageNumber = (number: number) => {
-        slideState.picNumber = number
-        setSlideState(slideState)
+        if (isAdmin) {
+            slideState.pageNumber = number
+            setSlideState(slideState)
+        } else {
+            const nextPageNumber = number
+            setIsSync(slideState.pageNumber === nextPageNumber)
+            setLocalPageNumber(nextPageNumber)
+        }
+    }
+
+    const syncSlide = () => {
+        setIsSync(true)
+        setLocalPageNumber(slideState.pageNumber)
     }
 
     return (
         <div className={style.main}>
 
-            {/* Adminコントロール */}
-            {admin ? <div>
+            {/* Adminコントロール(TODO: 消す) */}
+            {isAdmin ? <div>
                 <button onClick={goStart}>Back to beginning</button>
                 <button onClick={goPrevious}>Previous</button>
                 <button onClick={goNext}>Next</button>
@@ -56,15 +91,17 @@ const Page = () => {
             <div className={style.deck_space}>
                 <div>
                     <ImagePageView
-                        imageUrl={page?.imageUrl}
+                        imageUrl={viewingPage?.imageUrl}
                         identityId={slide?.createdBy}
                         onClickLeft={goPrevious}
                         onClickRight={goNext}
                     />
                     <SlideSlider
                         maxPageNumber={pages?.length || 0}
-                        pageNumber={slideState.picNumber}
+                        pageNumber={isSync ? slideState.pageNumber : localPageNumber}
                         onChangePageNumber={onChangePageNumber}
+                        isSync={isSync}
+                        syncSlide={syncSlide}
                     />
 
                 </div>
