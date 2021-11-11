aa713b703df82ddd83a1e4545c1a36b295c4b115
KitaharaMugiro <you@example.com>
2021-11-11 20:56:23 +0900

    construct html mail

--- src/lambda/mail/handler.py
+++ src/lambda/mail/handler.py
@@ -1,24 +1,35 @@
 import boto3
 import json
+import os
 
 
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 user_pool_id = "ap-northeast-1_WilzuDo5l"
-
+source_mail = "ngthornym@yahoo.co.jp"  # "no-reply@yunomy.com"
+htmls = {"ConfirmationForSubscribing": "ConfirmationForSubscribing.html"}
 # SESでメールを送信するためのハンドラ
 def lambda_handler(event, context):
-    user_name = None
+    USER_NAME = None
+    TYPE = None
+    TITLE = None
+    DATE = None
+    URL = None
     try:
         payload = json.loads(event["body"])
-        user_name = payload.get("user_name")
+        USER_NAME = payload.get("user_name")
+        TYPE = payload.get("type")
+        TITLE = payload.get("title") or "勉強会"
+        DATE = payload.get("date") or "1999/01/01 20:00 ~ 21:00"
+        URL = payload.get("url") or "https://yahoo.co.jp"
     except:
         raise Exception("Invalid payload")
 
-    if user_name is None:
+    if USER_NAME is None:
         raise Exception("Invalid payload")
 
     # cognitoからユーザー情報を取得
     client = boto3.client("cognito-idp")
-    response = client.admin_get_user(UserPoolId=user_pool_id, Username=user_name)
+    response = client.admin_get_user(UserPoolId=user_pool_id, Username=USER_NAME)
 
     attributes = response.pop("UserAttributes")
     print(attributes)
@@ -31,22 +42,48 @@ def lambda_handler(event, context):
     if email is None:
         raise Exception("Email is not registered")
 
+    # HTMLを構成
+    filename = htmls[TYPE]
+    filepath = os.path.join(__location__, filename)
+    with open(filepath) as f:
+        html_body = f.read()
+    if TYPE == "ConfirmationForSubscribing":
+        html_body = html_body.replace("$TITLE", TITLE)
+        html_body = html_body.replace("$DATE", DATE)
+        html_body = html_body.replace("$URL", URL)
+    print(html_body)
+
+    # メールタイトル
+    subject = None
+    if TYPE == "ConfirmationForSubscribing":
+        subject = f"【PresenShare】" + TITLE + "にご参加ありがとうございます"
+
     ses_client = boto3.client("ses")
     # メールを送信する
     ses_client.send_email(
-        Source="no-reply@yunomy.com",
+        Source=source_mail,
         Destination={
             "ToAddresses": [
                 email,
             ]
         },
         Message={
-            "Subject": {"Data": "テストメール", "Charset": "UTF-8"},
-            "Body": {"Text": {"Data": "テスト本文", "Charset": "UTF-8"}},
+            "Subject": {"Data": subject, "Charset": "UTF-8"},
+            "Body": {
+                "Text": {"Data": html_body, "Charset": "UTF-8"},
+                "Html": {"Data": html_body, "Charset": "UTF-8"},
+            },
         },
     )
 
 
 if __name__ == "__main__":
-    event = {"body": json.dumps({"user_name": "60f924f1-2617-4556-846f-e9770de6b0e3"})}
+    event = {
+        "body": json.dumps(
+            {
+                "user_name": "60f924f1-2617-4556-846f-e9770de6b0e3",
+                "type": "ConfirmationForSubscribing",
+            }
+        )
+    }
     lambda_handler(event, None)
