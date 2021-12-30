c8112626695b033017b609b7a0f2aa4ee8909598
KitaharaMugiro <you@example.com>
2021-12-30 22:42:42 +0900

    よくわからんけどpdf回収完了

--- src/lambda/pdf/ReadMe.md
+++ src/lambda/pdf/ReadMe.md
@@ -1,8 +1,26 @@
+# 所感
+Front -> Hasura -> Lambda(ECS) -> Hasuraという繋ぎなので、デバッグがかなりしんどい。
+
 # デプロイ
 ```
 cd src/lambda/pdf
+
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 904678698219.dkr.ecr.ap-northeast-1.amazonaws.com
+
 docker build -t pdf-to-png .
+
 docker tag pdf-to-png:latest 904678698219.dkr.ecr.ap-northeast-1.amazonaws.com/pdf-to-png:latest
+
 docker push 904678698219.dkr.ecr.ap-northeast-1.amazonaws.com/pdf-to-png:latest
 ```
+
+コンソールで新しいイメージをデプロイを実施しないといけない
+
+# ローカルテスト
+
+```
+docker build -t pdf-to-png .
+docker run -p 9000:8080 pdf-to-png
+```
+
+URL: http://localhost:9000/2015-03-31/functions/function/invocations
--- src/lambda/pdf/app.py
+++ src/lambda/pdf/app.py
@@ -5,16 +5,22 @@ from pdf2image import convert_from_bytes
 import boto3
 import PIL
 import requests
+import uuid
 
 # FROM https://qiita.com/yosiiii/items/bb7c6793b2bdd2029b95
+
+HASURA_URL = "https://adequate-guinea-56.hasura.app/v1/graphql"
 
 
 def lambda_handler(event, context):
     print("start lambda")
     body = json.loads(event["body"])
-    print(body)
+    headers = event["headers"]
+    print(event)
+    print(headers)
     input = body["input"]
     print(input)
+    slideId = input["slideId"]
     original_pdf_key = (
         "public/" + input["pdfName"]
     )  # amplifyが勝手にpublicをつけてkeyでそれを返してくれない
@@ -32,7 +38,6 @@ def lambda_handler(event, context):
 
     # pagesはPILのImageリストなのでbyteに変換
     index = 0
-    keys = []
     for page in pages:
         img_bytes = io.BytesIO()
         page.save(img_bytes, format="PNG")
@@ -44,7 +49,43 @@ def lambda_handler(event, context):
         s3.put_object(
             Body=img_bytes, Bucket=bucket_name, Key="public/" + key
         )  # amplifyはpublicしか読めないからpublicつける・・・
-        keys.append(key)
+
+        # APIコールでスライドページを作る
+        insertPageMutation = """
+            mutation InsertPage($object: slideshare_Page_insert_input!) {
+                insert_slideshare_Page_one(object: $object) {
+                    id
+                    pageNumber
+                    imageUrl
+                    isVisible
+                    slideId
+                    text
+                    type
+                    videoUrl
+                }
+            }
+        """
+        graphqlReq = {
+            "query": insertPageMutation,
+            "variables": {
+                "object": {
+                    "id": str(uuid.uuid4()),
+                    "slideId": slideId,
+                    "type": "image",
+                    "imageUrl": key,
+                    "pageNumber": index,
+                }
+            },
+        }
+        print("request")
+        print(graphqlReq)
+        res = requests.post(
+            url=HASURA_URL,
+            json=graphqlReq,
+            headers={"Authorization": headers["Authorization"]},
+        )
+        print("response")
+        print(res.text)
         index += 1
     print("process end")
-    return {"statusCode": 200, "body": json.dumps({"keys": keys})}
+    return {"statusCode": 200, "body": json.dumps({"slideId": slideId})}
