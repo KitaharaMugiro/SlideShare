259b027f9fdbf8ea58eaeba4423f8619251809f1
KitaharaMugiro <you@example.com>
2022-01-01 18:01:51 +0900

    古いコンポーネントの削除

--- pages/rooms/index.tsx
+++ pages/rooms/index.tsx
@@ -2,6 +2,7 @@
 import { useAtom } from "jotai";
 import { GetServerSideProps, GetStaticPropsContext } from "next";
 import React, { useEffect } from "react";
+import RoomAndPresentationTemplate from "../../components/rooms/RoomAndPresentationTemplate";
 import RoomTemplate from "../../components/rooms/RoomTemplate";
 import { DarkModeAtom } from "../../model/jotai/DarkMode";
 import OgpTag, { OpgMetaData } from "../../model/ogp/OgpTag";
@@ -9,14 +10,13 @@ import getOgpInfo from "../../model/serverSideRender/getOgpInfo";
 
 export default ({ ogpInfo }: { ogpInfo: OpgMetaData }) => {
     const [_, setThemeMode] = useAtom(DarkModeAtom)
-
     useEffect(() => {
         setThemeMode("light")
     }, [])
+
     return <div>
         <OgpTag ogpInfo={ogpInfo} />
-
-        <RoomTemplate />
+        <RoomAndPresentationTemplate />
     </div>
 }
 
